5a1d526537224a7b044a198c3b6650c4d6ea11c9
monicarlos <monica@getnea.com>
2021-06-17 14:00:38 -0500

    se eliminan las funciones: index, detail y results, y las librerias que no se utilizan

--- ejercicios_django/mysite/polls/views.py
+++ ejercicios_django/mysite/polls/views.py
@@ -1,27 +1,9 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
 from .models import Choice, Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
 
 
 class IndexView(generic.ListView):
