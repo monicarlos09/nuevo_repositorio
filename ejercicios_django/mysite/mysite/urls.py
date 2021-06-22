ccdf11c0e6d82c26157c85921de07f4d996b0f2f
monica <monica.getnea.com>
2021-06-22 11:39:33 -0500

    se corrigio los errores de login

--- ejercicios_django/mysite/mysite/urls.py
+++ ejercicios_django/mysite/mysite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 from django.contrib import admin
 #from django.urls import path
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', auth_views.LoginView.as_view()),
 ]
 
 # Add Django site authentication urls (for login, logout, password management)
