b65471823e95688ec69308d56933fc89eba7285f
monica <monica.getnea.com>
2021-07-07 11:25:27 -0500

    Se configura el archivo settings.py de la app para establecer la coneccion

--- ejercicios_django/mysite/mysite/settings.py
+++ ejercicios_django/mysite/mysite/settings.py
@@ -75,8 +75,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_polls',
+        'USER': 'user',
+        'PASSWORD': 'user',
+        'HOST': 'db',
+        'PORT': 3306,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'isolation_level': "read uncommitted"
+        },
     }
 }
 
