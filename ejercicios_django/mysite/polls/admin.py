f19d51d05c0bfea697a3e86ea9a95f1ebec9cc9e
monica <monica.getnea.com>
2021-07-07 12:24:54 -0500

    Se aplica guia de estilos

--- ejercicios_django/mysite/polls/admin.py
+++ ejercicios_django/mysite/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Choice, Question
+from .models import Choice
+from .models import Question
 
 
 class ChoiceInline(admin.TabularInline):
@@ -10,14 +11,13 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': [
-         'pub_date'], 'classes': ['collapse']}),
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    list_display = ("question_text", "pub_date", "was_published_recently")
 
 
 admin.site.register(Question, QuestionAdmin)
--- ejercicios_django/mysite/polls/tests.py
+++ ejercicios_django/mysite/polls/tests.py
@@ -1,20 +1,11 @@
-import datetime
-from django.contrib.auth.decorators import user_passes_test
-
-from django.test import TestCase
-from django.utils import timezone
-from django.urls import reverse
-
 from django.contrib.auth.models import User
-
-from .models import Question
+from django.test import TestCase
 
 
 class UserTests(TestCase):
     def setUp(self):
-        usuario = User.objects.create_user(
-            'test', 'admin@gmail.com', 'prueba123')
+        User.objects.create_user("test", "admin@gmail.com", "prueba123")
 
     def test_user_logeado(self):
-        logeado = User.objects.filter(user__name='test').exists()
+        logeado = User.objects.filter(user__name="test").exists()
         self.assertEqual(logeado, True)
